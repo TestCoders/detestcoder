d571a29586f4d027bd0832af962f9829a9bdda03
Marcel Blijleven <marcelblijleven@gmail.com>
2023-07-18 20:20:43 +0200

    chore: add usage examples

--- internal/ai/commands.go
+++ internal/ai/commands.go
@@ -3,7 +3,8 @@ package ai
 // SendPrompt is used to send the prompt to any AI backend
 func SendPrompt(service Service, prompt string) (*Response, error) {
 
-	// Do additional things that are backend independent here
+	// Do additional things that are backend independent here, for example
+	// validate config or output text to the terminal
 
 	return service.Send(prompt)
 }
